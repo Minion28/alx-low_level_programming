f9ebf6231bf3f1e29569ae8a19c2eb6da7ba1749
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-04-08 15:54:27 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 1)
 printf("%d is positive\n", n);
 else if (n < 0)
-printf("% is negative\n", n);
+printf("%d is negative\n", n);
 else
-printf("%D is zero\n", n);
+printf("%d is zero\n", n);
 return (0);
 }
