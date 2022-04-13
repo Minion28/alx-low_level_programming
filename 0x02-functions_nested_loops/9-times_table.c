424ad333ce0a3d50f49a36ba22b17683fcdfa964
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-04-13 03:59:43 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,22 +5,30 @@
  */
 void times_table(void)
 {
-int num, mult, prod;
-for (num = 0; num <= 9; num++)
+int i, j, prod;
+
+for (i = 0; i <= 9; i++)
 {
-for (mult = 1; mult <= 9; mult++)
+for (j = 0; j <=9; j++)
+{
+prod = (i*j);
+if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
-
-prod = num * mult;
-
-if (prod <= 9)
-_putchar(' ');
-else
+}
+if (prod >= 10)
+{
 _putchar((prod / 10) + '0');
-
-
+_putchar((prod % 10) + '0');
+}
+else if (prod < 10 && j !=0)
+{
+_putchar(' ');
+_putchar((prod % 10) + '0');
+}
+else
+{
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
