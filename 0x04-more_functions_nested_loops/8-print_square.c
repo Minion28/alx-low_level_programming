2186bfaead2d090adf287df9500d9bab00c54e7e
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-04-14 14:18:04 -0700

    mandatory tasks

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_square - print square
@@ -10,23 +10,21 @@
 
 void print_square(int size)
 {
-	int i, j;
+int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+if (size <= 0)
+{
+_putchar('\n');
 }
-
-
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
