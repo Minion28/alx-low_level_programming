87ff28de6a93de9d72b4140d404aa47f03b538b1
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-04-20 15:48:36 -0700

    advanced tasks

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,15 +4,29 @@
 #include <stdio.h>
 
 /**
- * my task
+ * main - random password generator
+ * Return: 0
  */
 
 int main(void)
 {
-int num;
+char a[100];
+int x, y, z;
+y = 0;
+z = 0;
+srand(time(NULL));
+while (y < 2465)
+{
+x = rand() % 122;
+if (x > 32)
+{
+a[z++] = x;
+y += r;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+}
+}
+a[z++] = (2772 - n);
+a[z] = '\0';
+printf("%s", a);
 return (0);
 }
