61f0a4497c369ca75c935651f15a877796878561
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-04-20 11:26:31 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_prev - print reverse
+ * @s: input string
+ *
+ * Description: prints a string, in reverse
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int left, len, l;
+len = 0;
+left = 0;
+for (l = 0; s[l] != '\0'; ++l)
+{
+len = l;
+}
+for (; len >= left; len--)
+{
+_putchar(s[len]);
+}
+_putchar('\n')
+}
