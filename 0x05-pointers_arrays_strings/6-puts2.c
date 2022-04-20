0efa9f42986f0ec3751d8b8aaaae1179bcaa5104
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-04-21 00:49:14 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,11 @@
 
 void puts2(char *str)
 {
-  int a;
-  
-  for (a = 0; str[a] != '\0'; a++)
-  {
-  if (a % 2 == 0)
-    _putchar (str[a]);
-  }
-  _putchar ('\n');
+int a; 
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
+_putchar ('\n');
 }
