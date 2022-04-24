813401556a55f6fda7dfe8427b56f1053b342c99
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-04-24 14:10:55 -0700

    tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,7 @@
 void print_number(int n)
 {
 unsigned int m, d, count;
+
 if (n < 0)
 {
 _putchar(45);
@@ -17,13 +18,16 @@ else
 {
 m = n;
 }
+
 d = m;
 count = 1;
+
 while (d > 9)
 {
 d /= 10;
 count *= 10;
 }
+
 for (; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
