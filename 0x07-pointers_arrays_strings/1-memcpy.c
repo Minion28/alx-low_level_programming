07d19bea9cfbff7cbd0aa463e78016495c8c3e6f
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-04-26 06:59:57 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * _memcpy - copy memory area
+ * @dest: destination of memory area
+ * @src: source of memory area
+ * @n: bytes filled
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+	*(dest + i) = *(src + i);
+
+return (dest);
+}
