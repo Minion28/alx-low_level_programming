b7ecf5b5caf8d9f81582d5a5cb9409cfd2be3c49
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-05-03 14:13:07 -0700

    tasks

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - Write the character c to stdout
- *
+ * _putchar - Writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
- *
  * On error, -1 is returned, and errno is set appropriately.
  */
 
