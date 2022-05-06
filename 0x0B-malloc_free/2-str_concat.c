606d7298c4a2c3afc64601997f73175af6e9b218
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-05-06 15:10:27 -0700

    tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,42 @@
-#include "main.h"
+a#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-/**
- * *str_concat - concanate 2 strings
- * @s1: first input string
- * @s2: second input string
- * Return: NULL if it fails
- */
 
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
+ */
 char *str_concat(char *s1, char *s2)
 {
-int s1_len, s2_len, x, y;
-char *str;
+char *strout;
+unsigned int i, j, k, limit;
+
 if (s1 == NULL)
-{
 s1 = "";
-}
-else if (s2 == NULL)
-{
+if (s2 == NULL)
 s2 = "";
-}
-for (s1_len = 0; s1[s1_len] != '\0\; s1_len++)
-continue;
-for (s2_len = 0; s2[s2_len] != '\0\; s2_len++)
-continue;
-str = malloc(s1_len + s2_len + 1);
-if (str == NULL)
+
+for (i = 0; s1[i] != '\0'; i++)
+	;
+
+for (j = 0; s2[j] != '\0'; j++)
+	;
+
+strout = malloc(sizeof(char) * (i + j + 1));
+if (strout == NULL)
 {
+free(strout);
 return (NULL);
 }
-x = 0;
-while (x < s1_len)
-{
-str[x] = s1[x];
-x++;
-}
-y = 0;
-while (i < (s1_len + s2_len))
-{
-str[x] = s2[j];
-x++;
-y++;
-}
-str[x] = '\0';
-return (str);
+
+for (k = 0; k < i; k++)
+strout[k] = s1[k];
+
+limit = j;
+for (j = 0; j <= limit; k++, j++)
+strout[k] = s2[j];
+
+return (strout);
 }
