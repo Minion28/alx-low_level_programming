d0a847a378b07abbd5f88d7faceb570611275702
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-05-24 20:07:37 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,9 @@
  *Return: 0
  */
 
+void __attribute__ ((constructor)) the();
 void the(void)
 {
-printf("You're beat! and yet, you must allow,\n");
-printf(	"I bore	my house upon my back!\n");
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
 }
