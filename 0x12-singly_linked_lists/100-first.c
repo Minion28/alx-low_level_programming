0f8fb65a7d53a9e48eb092b6ccf6f559288ea123
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-05-24 19:59:51 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * the - prints a specific string
+ *Return: 0
+ */
+
+void the(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf(	"I bore	my house upon my back!\n");
+}
