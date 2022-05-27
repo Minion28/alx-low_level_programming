94327bd506ec21564bc859606203fabac7479ce0
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-05-27 14:22:19 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free listint_t list
+ * @head: header node
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *a;
+if (head == NULL)
+{
+return;
+}
+for (; *head != NULL;)
+{
+a =(*head)->next;
+free(*head);
+*head = a;
+}
+head = NULL;
+}
