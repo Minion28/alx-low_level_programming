cde6392355e6be6667d93651971ddba32aefb42a
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-06-01 10:49:43 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+unsigned int a;
+a = 1;
+char *c = (char *) &a;
+return ((int)*c);;
+}
