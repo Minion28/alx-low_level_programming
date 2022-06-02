907a01b5298c18a96142e2bdb84c855775683d17
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-06-02 12:03:55 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,25 +14,30 @@ int y;
 int z;
 char *a;
 a = malloc(sizeof(char) * letters);
+
 if (!filename)
 {
 return (0);
 }
+
 x = open(filename, O_RDONLY);
 if (x < 0)
 {
 return (0);
 }
+
 if (!a)
 {
 return (0);
 }
+
 y = read(x, a, letters);
 if (y < 0)
 {
 free(a);
 return (0);
 }
+
 a[y] = '\0';
 close(x);
 z = write(STDOUT_FILENO, a, y);
@@ -41,6 +46,7 @@ if (z < 0)
 free(a);
 return (0);
 }
+
 free(a);
 return (z);
 }
