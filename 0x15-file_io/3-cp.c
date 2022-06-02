6c21da88e8c5d546f7dba943fdf4710698d07f73
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-06-03 02:18:33 +0300

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * main - copy content of one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+int x;
+int y;
+int z;
+int a;
+int b;
+char c[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+}
+x = open(argv[1], O_RDONLY);
+if (x < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+y = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+while ((z = read(x, c, 1024)) > 0)
+{
+if (y < 0 || (write(y, c, z) != z))
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
+}
+if (z < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+a = close(x);
+if (a < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", x), exit(100);
+}
+b = close(y);
+if (b < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", y), exit(100);
+}
+return (0);
+}
