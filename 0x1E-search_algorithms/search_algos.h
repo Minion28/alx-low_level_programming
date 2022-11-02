4eb8b83f38026bbc73203f401c59be840c465c5a
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-11-02 23:56:44 +0300

    prototypes and headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,10 +9,4 @@ int linear_search(int *array, size_t size, int value);
 /*1-binary search*/
 int binary_search(int *array, size_t size, int value);
 
-/*7-jump search*/
-int jump_search(int *array, size_t size, int value);
-
-/*9-interpol search*/
-int interpolation_search(int *array, size_t size, int value);
-
 #endif
